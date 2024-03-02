85b656c35c61ee61975c9a895fdf8be93ed6b97b
Nikx908 <nikhilparbat908@gmail.com>
2024-03-03 03:07:08 +0530

    serializers added

--- ChatSystem/chat/serializers.py
+++ ChatSystem/chat/serializers.py
@@ -1,1 +1,15 @@
+# serializers.py
 from rest_framework import serializers
+
+class MessageSerializer(serializers.Serializer):
+    message = serializers.CharField()
+
+
+
+# urls.py
+from django.urls import path
+from .views import ChatAPIView
+
+urlpatterns = [
+    path('api/chat/<str:room_name>/', ChatAPIView.as_view(), name='chat-api'),
+]
